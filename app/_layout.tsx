5797dc3f68df33fea108f903634bc1fbe97cb1c9
Parth Bhanushali <52828668+Parth-Bhanushali@users.noreply.github.com>
2024-05-18 13:46:04 +0530

    Setup client using provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
 import { Stack } from "expo-router";
+import {ApolloProvider} from '@apollo/client';
+import client from '../network/client'
+
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerTitle: "Home" }} />
-    </Stack>
+    <ApolloProvider client={client}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerTitle: "Home" }} />
+      </Stack>
+    </ApolloProvider>
   );
 }
